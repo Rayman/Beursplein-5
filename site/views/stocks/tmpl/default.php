b09e61391e296bfec882f354708f26ebff3e27ec
Rayman <rayman747@hotmail.com>
2009-07-05 17:36:36 +0200

    Refractored the stocks view

--- site/views/stocks/tmpl/default.php
+++ site/views/stocks/tmpl/default.php
@@ -8,7 +8,80 @@ defined('_JEXEC') or die('Restricted access'); ?>
   method="post"
   onsubmit="validateInput()">
 
-<?php echo $this->stocksTable;?>
+<table border="1" style="border-collapse: collapse;">
+  <tr>
+    <th>Aandeel</th>
+    <th>Waarde</th>
+    <th>Verandering</th>
+    <th>Gekocht</th>
+    <th>Volume</th>
+    <th>Actie</th>
+  </tr>
+<?php
+$javascriptPriceList = "priceList = new Array();\n";
+
+foreach($this->stocksList as $stock)
+{?>
+  <tr>
+    <td>
+      <img src="<?php echo $stock['image'];?>" height="30" alt="<?php echo $stock['name'];
+  echo '" />';
+  echo $stock['name'];?>
+
+    </td>
+    <td><?php echo $stock['value'];?></td>
+<?php 
+  //Color
+  if($stock['change']>0)
+  {
+    $color = "green";
+  }
+        elseif($stock['change']<0)
+  {
+    $color = "red";
+  }
+        else
+  {
+    $color = "blue";
+  }
+      
+  //Change in %
+  $changep = round(((float)$stock['change']*100)/
+  (((float)($stock['value']) - $stock['change'])));
+    
+  //Add the '+' sign
+  $changep = $stock['change'] >= 0 ? "+".(string)$changep     : $changep;
+  $change  = $stock['change'] >= 0 ? "+".(string)$stock['change'] : $stock['change'];
+  echo "    <td><span style=\"color: $color\">{$change} ({$changep}%)</span></td>\r\n";
+      
+  //Gekocht
+  $stock_bought = $this->userStocks[$stock['id']];
+  $amount = isset($stock_bought) ? $stock_bought['amount'] : 0;
+  echo   "    <td>{$amount}</td>\r\n";
+  
+  //Volume
+  $stock_bought = $this->totalStocks[$stock['id']];
+  $amount = isset($stock_bought) ? $stock_bought['amount'] : 0;
+  echo   "    <td>{$amount}</td>";
+?>
+
+  <td>
+    <input type="text" size="10" name="stock[<?php 
+      echo $stock['id'];?>]" value="" onchange="updateMoney(this)" />
+    <label>
+      <input type="radio" name="options[<?php
+        echo $stock['id'];?>]" value="Buy"  onchange="updateMoney()" />Koop
+    </label>
+    <label>
+      <input type="radio" name="options[<?php 
+        echo $stock['id'];?>]" value="Sell" onchange="updateMoney()" />Verkoop
+    </label>
+    </td>
+  </tr>
+<?php
+}
+echo "</table>";
+?>
 
   <input type="submit" value="Koop &amp; Verkoop" onclick="return validateInput()" />
   <p>
@@ -22,7 +95,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 money = <?php echo $this->money;?>;
 document.getElementById('money').innerHTML = money;
 
-<?php echo $this->javascriptPriceList;?>;
+<?php echo $this->javascriptPriceList;?>
 
 /*
 * Search for an option with name
--- site/views/stocks/view.html.php
+++ site/views/stocks/view.html.php
@@ -16,8 +16,19 @@ class BeurspleinViewStocks extends JView
   {
     //Get the stocks
     $stocksList  = $this->get( 'StocksList', 'Stocks' );
+    $this->assignRef('stocksList', $stocksList);
+    
+    $javascriptPriceList = "";
+    foreach($stocksList as $stock)
+    {
+      $javascriptPriceList .= "priceList['stock[{$stock['id']}]'] = {$stock['value']};\n";
+    }
+    $this->assignRef('javascriptPriceList', $javascriptPriceList);
+      
+    
     //Get all bought stocks
     $totalStocks = $this->get( 'TotalStocks', 'Portfolio');
+    $this->assignRef('totalStocks', $totalStocks);
     
     //Get the users stocks
     $list = $this->get( 'StocksList', 'Portfolio');
@@ -25,7 +36,7 @@ class BeurspleinViewStocks extends JView
     //TODO:
     //Combine these, so only 1 query has to be done
     
-    //Transform the userStocks
+    //Add amounts together
     $userStocks = array();
     foreach($list as $stock)
     {
@@ -37,90 +48,13 @@ class BeurspleinViewStocks extends JView
       {
 	$userStocks[$stock['stock_id']]['amount'] = $stock['amount'];
       }
-    }
-    
-    //Build the table
-    $stocksTable  = "<table border=\"1\" style=\"border-collapse: collapse;\">\r\n";
-    $stocksTable .= "\t<tr>\r\n";
-    $stocksTable .= "\t\t<th>Aandeel</th>\r\n";
-    $stocksTable .= "\t\t<th>Waarde</th>\r\n";
-    $stocksTable .= "\t\t<th>Verandering</th>\r\n";
-    $stocksTable .= "\t\t<th>Gekocht</th>\r\n";
-    $stocksTable .= "\t\t<th>Volume</th>\r\n";
-    $stocksTable .= "\t\t<th>Actie</th>\r\n";
-    $stocksTable .= "\t</tr>\r\n";
-    
-    $javascriptPriceList = "priceList = new Array();\n";
-    
-    foreach($stocksList as $stock)
-    {
-      $stocksTable .= "\t<tr>\r\n";
-      
-      //Aandeel
-      $stocksTable .= "\t\t<td>\r\n\t\t\t<img src=\"{$stock['image']}\"
-        height=\"30\" alt=\"{$stock['name']}\"/> \r\n";
-      $stocksTable .= "\t\t\t".$stock['name']."\r\n\t\t</td>\r\n";
-      
-      //Waarde
-      $stocksTable .= "\t\t<td>{$stock['value']}</td>\r\n";
-      
-      //Verandering      
-      //Color
-      if($stock['change']>0)
-      {
-        $color = "green";
-      }
-      elseif($stock['change']<0)
-      {
-        $color = "red";
-      }
-      else
-      {
-        $color = "blue";
-      }
-      
-      //Change in %
-      $changep = round(((float)$stock['change']*100)/
-          (((float)($stock['value']) - $stock['change'])));
-      
-      //Add the '+' sign
-      $changep = $stock['change'] >= 0 ? "+".(string)$changep     : $changep;
-      $change  = $stock['change'] >= 0 ? "+".(string)$stock['change'] : $stock['change'];
-      
-      //Draw it
-      $stocksTable .= "\t\t<td><span style=\"color: $color\">{$change} ({$changep}%)</span></td>\r\n";
-      
-      //Gekocht
-      $stock_bought = $userStocks[$stock['id']];
-      $amount = isset($stock_bought) ? $stock_bought['amount'] : 0;
-      $stocksTable .= "\t\t<td>{$amount}</td>\r\n";
-      
-      //Volume
-      $stock_bought = $totalStocks[$stock['id']];
-      $amount = isset($stock_bought) ? $stock_bought['amount'] : 0;
-      $stocksTable .= "\t\t<td>{$amount}</td>\r\n";
-      
-      //Actie
-      $stocksTable .= "\t\t<td>\r\n\t\t\t<input type=\"text\" size=\"10\"
-        name=\"stock[{$stock['id']}]\" value=\"\" onchange=\"updateMoney(this)\" />\r\n";
-      $stocksTable .= "\t\t\t<label><input type=\"radio\" name=\"options[{$stock['id']}]\"
-        value=\"Buy\"  onchange=\"updateMoney()\" />Koop</label>\r\n";
-      $stocksTable .= "\t\t\t<label><input type=\"radio\" name=\"options[{$stock['id']}]\"
-        value=\"Sell\" onchange=\"updateMoney()\" />Verkoop</label>\r\n\t\t</td>\r\n";
-      
-      $stocksTable .= "\t</tr>\r\n";
-      
-      $javascriptPriceList .= "priceList['stock[{$stock['id']}]'] = {$stock['value']};\n";
-    }
-    
-    $stocksTable .= "</table>";
-    $this->assignRef( 'stocksTable', $stocksTable );
-    
+    }    
+    $this->assignRef('userStocks', $userStocks);
+     
+    //Get the user's money
     $money = $this->get('Money', 'Users');
     $this->assignRef('money', $money);
     
-    $this->assignRef('javascriptPriceList', $javascriptPriceList);
-
     parent::display($tpl);
   }
 }
