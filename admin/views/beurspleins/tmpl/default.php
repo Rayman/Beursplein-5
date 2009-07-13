af66c39fb0640b0afd1846d6166f046c707a3d4b
Rayman <rayman747@hotmail.com>
2009-07-13 15:49:45 +0200

    Removed user and card manager
    They aren't needed anyway

--- admin/views/beurspleins/tmpl/default.php
+++ admin/views/beurspleins/tmpl/default.php
@@ -2,8 +2,6 @@
 <h1>Wat wil je doen?</h1>
 
 <ul>
-<li><a href="index.php?option=com_beursplein&amp;view=stocks">Aandelen manager</a></li>
-<li><a href="index.php?option=com_beursplein&amp;view=users">User manager</a></li>
-<li><a href="index.php?option=com_beursplein&amp;view=cards">Cards manager</a></li>
+  <li><a href="index.php?option=com_beursplein&amp;view=stocks">Aandelen manager</a></li>
 </ul>
 
